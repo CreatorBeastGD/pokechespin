684b10271c6c1d8400cd4ea78e8d434ae075db53
Javier Molina Colmenero <javiskorpion@gmail.com>
2024-12-31 19:58:07 +0100

    Reapply "version 1.0"
    This reverts commit cfc0178b9974043bbdfe18e6924f9686e47bf295.

--- src/components/search-bar-attacker.tsx
+++ src/components/search-bar-attacker.tsx
@@ -196,7 +196,7 @@ export default function SearchBarAttacker({ onSelect, onQuickMoveSelect, onCharg
             />
           )}
           <div>
-            <p>Stat picker</p>
+            <p>Stat picker <span className="italic text-xs">(You can slide to select your desired data!)</span> </p>
             <p>Level: {stats[0]}</p>
             <Slider onValueChange={(value) => handleChangeStat(value, 0)} defaultValue={[50]} max={51} step={0.5} min={1} className="w-[60%] mb-1" color={stats[0] == 51 ? "bg-blue-500" : "bg-blue-700"}/>
             <p className={stats[1] == 15 ? "text-red-600" : "text-yellow-600"}>Attack: </p>
@@ -205,6 +205,7 @@ export default function SearchBarAttacker({ onSelect, onQuickMoveSelect, onCharg
             <Slider onValueChange={(value) => handleChangeStat(value, 2)} defaultValue={[15]} max={15} step={1} className="w-[60%] mb-1" color={stats[2] == 15 ? "bg-red-500" : "bg-yellow-600"}/>
             <p className={stats[3] == 15 ? "text-red-600" : "text-yellow-600"}>Stamina: </p>
             <Slider onValueChange={(value) => handleChangeStat(value, 3)} defaultValue={[15]} max={15} step={1} className="w-[60%] mb-5" color={stats[3] == 15 ? "bg-red-500" : "bg-yellow-600"}/>
+            
           </div>
           <div className="flex flex-row space-x-4">
             <div>
