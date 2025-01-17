a7dc6042835f7159755cd0252645686e69f47a2b
Javier Molina Colmenero <javiskorpion@gmail.com>
2025-01-17 20:01:53 +0100

    Create page.tsx

---
+++ app/pokemon-go-damage-calculator/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Page() {
+    return (
+        <div className="flex flex-col items-center justify-center space-y-2 mt-4">
+            <img src="/favicon.ico" alt="Favicon" className="inline-block mr-2 favicon" />
+            <h1 className="text-white text-2xl">Pokémon GO Damage Calculator</h1>
+            <p className="text-white text-lg">Calculate the damage output of your Pokémon in Pokémon GO! Simulate battles and raids, and find out the best moveset for your Pokémon.</p>
+        </div>
+    );
+}
